f9e0541ef56d53cd2fc5bb6f8508e604bf9eeb1c
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2015-11-27 14:34:08 -0500

    get documents by their ids

--- question2query.py
+++ question2query.py
@@ -5,28 +5,39 @@ the initial information need and will produce good results (that contain an answ
 
 from elasticsearch import Elasticsearch as ES
 from elasticsearch.client import CatClient as cat
+import random
 
-def chooseRandomQuestions(index_name, n):
-    '''
-    From a given index choose n random questions.
-    We use them to test how well questions to query works.
-    :param index_name: name of an index in ES
-    :param n: how many questions would you like to pick
-    :return: a list of questions
-    '''
-    index_name = 'yahoo'
+class ESHelper:
+    def __init__(self):
+        self.elasticClient = ES()
 
-    # get index length in documents - L
-    # produce n random numbers in range 1:L
-    # return either a list of ids or a list of actual questions
-    elastic = ES()
+    def chooseRandomQuestions(self, index_name, n):
+        '''
+        From a given index choose n random questions.
+        We use them to test how well questions to query works.
+        :param index_name: name of an index in ES
+        :param n: how many questions would you like to pick
+        :return: a list of questions
+        '''
 
-    total_docs = int(elastic.count(index=index_name).get('count', -1))
-    if total_docs == -1:
-      print('Could not fetch document count. Failed.')
-      return
-    print(type(total_docs))
-    print(total_docs)
+        # get index length in documents - L
+        # produce n random numbers in range 1:L
+        # return either a list of ids or a list of actual questions
+        elastic = ES()
 
+        # total_docs = int(elastic.count(index=index_name).get('count', -1))
+        total_docs = int(self.elasticClient.count(index=index_name).get('count', -1))
+        if total_docs == -1:
+            print('Could not fetch document count. Failed.')
+            return
 
-chooseRandomQuestions('yahoo', 100)
+        test_sample = random.sample(range(1, total_docs + 1), n)
+        return test_sample
+
+    def getDocumentsByIds(self, ids, index_name='yahoo', doc_type='qapair'):
+        for doc_id in ids:
+            single_doc = self.elasticClient.get(index=index_name, doc_type=doc_type, id=doc_id)
+            print(single_doc)
+
+eshelper = ESHelper()
+eshelper.chooseRandomQuestions('yahoo', 100)
