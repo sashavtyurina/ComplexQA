112ad5ce0988d15b19a5ecb2167ea0c18c31e77e
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2015-12-20 17:47:38 -0500

    added entropy test

--- entropy.py
+++ entropy.py
@@ -10,7 +10,12 @@ The next step is to look at entropy distributions. And this is what I'm going to
 from question2query import ESHelper, TOTAL_TOKENS, CommonUtils
 
 def entropy(qid, index_name, doc_type):
+    f = open('question_entropy.txt', 'a')
+
     es = ESHelper()
+
+    doc = es.getDocumentsByIds([qid], index_name, doc_type)
+
     stats = es.statistics4docid(index_name, doc_type, qid, add_fields=['best'])
 
     body_stats = stats['term_vectors']['body']['terms']
@@ -18,7 +23,6 @@ def entropy(qid, index_name, doc_type):
     question_stats = CommonUtils.mergeDicts(body_stats, title_stats)
 
     answers_stats = stats['term_vectors']['answers']['terms']
-
     best_answer_stats = stats['term_vectors']['best']['terms']
 
     question_entropy = 0
@@ -32,5 +36,14 @@ def entropy(qid, index_name, doc_type):
 
         question_entropy += term_entropy
 
+    f.write('***\nQuestion:\n%s\nEntropy: %f' % (doc['_source'], question_entropy))
+    print(doc['_source'])
+    print('Question entropy = %f' % question_entropy)
+    input()
+    f.close()
+
+
+for i in range(1, 10):
+    entropy(i, 'yahoo_', 'qa')
 
 
